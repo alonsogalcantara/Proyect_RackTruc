6de722ae8d7517b2562b827188d3edc7b45d068b
unknown <rodrigoalvarezpicen@gmail.com>
2020-10-03 04:13:28 -0500

    se realizo el header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './index.css';
-import DetallesCamionero from './Componentes/detallesCamionero';
-import RegistrarUsuario from './Componentes/registrarUsuario';
+//import DetallesCamionero from './Componentes/detallesCamionero';
+//import RegistrarUsuario from './Componentes/registrarUsuario';
+import HeaderPagina from './Componentes/headerPagina';
+
 
 export default function App() {
   return (
     <div>
-     <RegistrarUsuario/>
-     <DetallesCamionero/>
+      <HeaderPagina/>
     </div>
   );
 }
---
+++ src/Componentes/headerPagina.js
@@ -0,0 +1,40 @@
+import React from 'react'
+class headerPagina extends React.Component{
+
+    constructor(busq) {
+        super();
+        this.state = {
+            name: '',
+        };
+
+        this.nameHandler = this.nameHandler.bind(this);
+    }
+
+
+    nameHandler(event) {
+        this.setState({ name: event.target.value });
+    }
+
+    render(){
+        return <div className = "barraBuscadora" >
+                <form>
+          
+                 
+                    <div class="field" id="searchform">
+                    <img src="logo192.png" style={{height:30, width: 30}} hspace="25"/>
+                    <input type="text" id="Busqueda" placeholder="" style={{height:20, width:300}}/> <button type="button" id="Buscar">BUSCAR</button>
+                    <text>ID</text>        
+                    <text>/</text> 
+                    <text>Nombre</text> 
+                    <text>/</text> 
+                    <text>Fecha</text>  
+                    
+                    </div> <script class="cssdeck" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+
+
+                </form>
+        </div>
+    }
+}
+
+export default headerPagina;
