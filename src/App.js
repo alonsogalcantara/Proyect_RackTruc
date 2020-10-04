13cb3598e9f8f7e376fd0dab2b1773f2e25cc335
Yesss <denzel1999149@gmail.com>
2020-10-04 16:39:38 -0500

    agrego modificaciones detallesCamioneros

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './index.css';
 import headerPagina from './Componentes/headerPagina'
 import RegistrarUsuario from './Componentes/registrarUsuario';
-
+import DetallesCamionero from './Componentes/detallesCamionero';
 export default function App() {
   return (
     <div>
-      <headerPagina/>
+     <headerPagina/>
      <RegistrarUsuario/>
+     <DetallesCamionero/>
     </div>
   );
 }
