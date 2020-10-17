b36cac06446a28ad765988e8fc911375d967a110
unknown <rodrigoalvarezpicen@gmail.com>
2020-10-17 05:35:44 -0500

    Se inicio el archivo ver Camiones

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import mainAdmin from "./Componentes/mainAdmin";
 import detallesCamionero from "./Componentes/detallesCamionero";
 import detallesCamion from "./Componentes/detallesCamion";
 import registrarUsuario from "./Componentes/registrarUsuario";
+import verCamiones from "./Componentes/verCamiones";
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
         <Route exact path="/detallesCamionero" component={detallesCamionero} />
         <Route exact path="/detallesCamion" component={detallesCamion} />
         <Route exact path="/registrarUsuario" component={registrarUsuario} />
+        <Route exact path="/verCamiones" component={verCamiones}/>
       </Switch>
     </BrowserRouter>
   );
